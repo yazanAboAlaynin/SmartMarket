2a095491c604f95ef50f916397b3540f2f61cc67
yazan abo alaynin <yazanmohned@gmail.com>
2020-03-15 18:45:00 +0200

    creating models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->Date('dob');
+            $table->string('mobile');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
