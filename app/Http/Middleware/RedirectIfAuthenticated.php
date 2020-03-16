2828fbe5a4bf0ccdb038f7722ccdb70bc9c3db8b
yazan abo alaynin <yazanmohned@gmail.com>
2020-03-16 18:49:36 +0200

    admin login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            if($guard === 'admin') {
+                return redirect('/admin/home');
+            }
+            return redirect('/home');
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::namespace('Admin')->prefix('admin')->as('admin.')->group(function() {
+    Auth::routes(['register' => false]);
+    Route::get('/home', 'HomeController@index')->name('home');
+});
