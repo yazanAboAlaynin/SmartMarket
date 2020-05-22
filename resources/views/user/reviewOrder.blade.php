f3511e3b8ff39d49cd8497f906cc9fa531dd0ef2
MohammadSulaiman97 <mohammadsulaiman870@gmail.com>
2020-05-22 16:14:38 +0300

    latest

--- resources/views/user/reviewOrder.blade.php
+++ resources/views/user/reviewOrder.blade.php
@@ -8,23 +8,23 @@
             <div class="row">
                 <div class="col-md-12">
 
-                        <div class="case">
-                            <div class="row">
-                                <div class="col-md-6 col-lg-6 col-xl-8 d-flex">
-                                    {{--<a href="" class="img w-100 mb-3 mb-md-0" style="background-image: url({{ asset('/storage/' . $order->order_items->product->image) }});"></a>--}}
-                                </div>
-                                <div class="col-md-6 col-lg-6 col-xl-4 d-flex">
-                                    <div class="text w-100 pl-md-3">
-                                        <span class="subheading">{{$o->discount}}</span>
-                                        <h2><a href="">mm</a></h2>
+                    <div class="case">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-6 col-xl-8 d-flex">
+                                {{--<a href="" class="img w-100 mb-3 mb-md-0" style="background-image: url({{ asset('/storage/' . $order->order_items->product->image) }});"></a>--}}
+                            </div>
+                            <div class="col-md-6 col-lg-6 col-xl-4 d-flex">
+                                <div class="text w-100 pl-md-3">
+                                    <span class="subheading">{{$o->discount}}</span>
+                                    <h2><a href="">mm</a></h2>
 
-                                        <div class="meta">
-                                            <p class="mb-0"><a href="#">Add Review</a></p>
-                                        </div>
+                                    <div class="meta">
+                                        <p class="mb-0"><a href="#">Add Review</a></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
                 </div>
             </div>
